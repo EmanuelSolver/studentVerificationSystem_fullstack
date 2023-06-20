8173f987569ef79b771d5ca7755a3a2c2e720c82
Emanuel Solver <emanuelmutuiri@gmail.com>
2023-06-20 13:08:59 +0300

    still on homepage

--- ClientSide/src/pages/Home.jsx
+++ ClientSide/src/pages/Home.jsx
@@ -11,14 +11,16 @@ const Home = () => {
           </video>
 
         </div>
+
         <div className="container">
-        
+          <h1 className="heading">Welcome to ourCollege</h1>
+
           <div className="content">
-          <img src="students.jpg" alt="image" className="image" />
-            <h1 className="heading">Welcome to ourCollege</h1>
-            <p className="description">Creativity convergence</p>
+            <img src="students.jpg" alt="image" className="image" />
+            <p className="sub-head">Learn with us</p> <br />
             <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quisquam, cum excepturi fuga eveniet sapiente sit? Repellat nostrum reprehenderit necessitatibus, quasi, excepturi, ipsa assumenda magnam incidunt obcaecati facilis enim perspiciatis? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti qui nemo beatae consectetur laboriosam voluptates ipsam natus blanditiis enim, in ducimus atque officiis? Adipisci, facilis iste labore impedit totam repellat!</span>
-            <br /><button className="button">Visit Us</button>
+            <br />
+            <button className="button">Visit Us</button>
           </div>
         </div>
     </>
